d96759d6ea864e567b90252607c97a0f320d51a4
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-06-25 17:19:47 +1000

    set autocapture posthog

--- fastregexgenerator/src/app/providers.tsx
+++ fastregexgenerator/src/app/providers.tsx
@@ -7,6 +7,13 @@ if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
+    autocapture: {
+      dom_event_allowlist: ["click"], // DOM events from this list ['click', 'change', 'submit']
+      url_allowlist: ["posthog.com./docs/.*"], // strings or RegExps
+      element_allowlist: ["button"], // DOM elements from this list ['a', 'button', 'form', 'input', 'select', 'textarea', 'label']
+      css_selector_allowlist: ["[ph-autocapture]"], // List of CSS selectors
+      element_attribute_ignorelist: ['data-attr-pii="email"'], // List of element attributes to ignore
+    },
   });
 }
 
