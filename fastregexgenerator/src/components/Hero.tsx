4ea9e8e449e3d1a2cf4b698908fa162fc523c16c
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-06-25 17:50:29 +1000

    product hunt badge

--- fastregexgenerator/src/components/Hero.tsx
+++ fastregexgenerator/src/components/Hero.tsx
@@ -16,6 +16,18 @@ const Hero = () => {
       <p className="text-md md:text-md mb-8 font-semibold text-center max-w-xl text-neutral-400">
         Regex sucks. Simplify it with AI, so you can focus on meaningful work.
       </p>
+      <a
+        href="https://www.producthunt.com/posts/magic-regex-generator?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-magic&#0045;regex&#0045;generator"
+        target="_blank"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=465742&theme=light"
+          alt="Magic&#0032;Regex&#0032;Generator - Generate&#0032;and&#0032;test&#0032;regex&#0032;with&#0032;AI | Product Hunt"
+          className="w-[250px] h-[54px] my-3"
+          width="250"
+          height="54"
+        />
+      </a>
       <ArrowDownIcon className=" animate-bounce text-neutral-200 size-10 my-3" />
     </div>
   );
